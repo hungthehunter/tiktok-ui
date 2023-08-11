ef7ea4709dda0e773cc6c1b6ed098c7e0f6444d4
Quoc Hung <lenguyenquochunghello@gmail.com>
2023-08-11 22:15:15 +0700

    tiktok clone

---
+++ reactjs/tiktok-ui/src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import myAlert from "~/myAlert"
+
+function Button(){
+    return(
+    <div>
+    <button onClick={myAlert}>onCLick</button>
+    </div>
+    )
+}
+
+export default Button
---
+++ reactjs/tiktok-ui/src/myAlert.js
@@ -0,0 +1,4 @@
+const myAlert=()=>{
+    alert("hello")
+}
+export default myAlert
